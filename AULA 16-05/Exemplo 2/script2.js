34b9cdf0f45a6f8fb288424f448e06be1da51360
RYANALVESLOPES <tad-ryanlopes@ucpparana.edu.br>
2024-05-16 21:20:14 -0300

    bora

--- AULA 16-05/Exemplo 2/script2.js
+++ AULA 16-05/Exemplo 2/script2.js
@@ -1,18 +1,32 @@
 const elemento = document.querySelectorAll("p");
 
-elemento.firstEach(function(teste){
+elemento.forEach(function(teste){
     console.log(teste);
 });
 
-const alerta = document.querySelector(". btn-primary");
+const alerta = document.querySelector(".btn-primary");
 
 alerta.addEventListener("click", function(){
-    alerta("Funcionou???");
+    alert("Funcionou???");
 });
 
-const escuro = document.querySelector(".btn-btn-dark");
 
+
+const escuro = document.querySelector(".btn-dark");
+ 
 escuro.addEventListener("click", function(){
     const pagina_escuro = document.querySelector("body");
-    pagina_escuro.style.background("black");
+    pagina_escuro.style.backgroundColor = "black";
+    pagina_escuro.style.color = "white";
 });
+
+
+const branco = document.querySelector(".btn-light");
+ 
+branco.addEventListener("click", function(){
+    const pagina_claro = document.querySelector("body");
+    pagina_claro.style.backgroundColor = "white";
+    pagina_claro.style.color = "black";
+});
+
+
