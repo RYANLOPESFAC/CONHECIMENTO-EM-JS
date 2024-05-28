3505e48ea95e62399b8aa84834bdd369fcadf36b
RYANALVESLOPES <tad-ryanlopes@ucpparana.edu.br>
2024-05-27 21:31:09 -0300

    suba

---
+++ AULA 16-05/script.js
@@ -0,0 +1,9 @@
+const elemento = document.querySelector("p");
+
+const elemento_class = document.querySelector(".container");
+
+const elemento2 = document.querySelector("p");
+
+const elemento_class2 = document.querySelector(".container");
+
+
